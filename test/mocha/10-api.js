b68807ccfa8d16422fb5e9f5c0a26856fc62183f
Matthew Collier <mcollier@digitalbazaar.com>
2021-03-30 18:13:09 -0400

    Update did-io API usage.

--- test/mocha/10-api.js
+++ test/mocha/10-api.js
@@ -6,14 +6,6 @@ const {didIo} = require('bedrock-did-io');
 describe('bedrock-did-io API', () => {
   it('should have proper exports', async () => {
     should.exist(didIo);
-    didIo.should.be.an('object');
-    didIo.should.have.keys(['methods']);
-    didIo.methods.should.be.an('object');
-    didIo.methods.should.have.keys(['key', 'v1']);
-    didIo.methods.key.should.be.an('object');
-    didIo.methods.key.method.should.equal('key');
-    didIo.methods.v1.should.be.an('object');
-    didIo.methods.v1.method.should.equal('v1');
-    didIo.methods.v1.mode.should.equal('test');
+    didIo.get.should.be.a('function');
   });
 });
